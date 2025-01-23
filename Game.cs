eba49f2fcb2c96cbcac6426b9fcd510f8c23bf9f
TrinityHaig <000915272@mohawkcollege.ca>
2025-01-23 11:23:25 -0500

    Add project files.

---
+++ Game.cs
@@ -0,0 +1,36 @@
+﻿// Include the namespaces (code libraries) you need below.
+using System;
+using System.Numerics;
+
+// The namespace your code is in.
+namespace MohawkGame2D
+{
+    /// <summary>
+    ///     Your game code goes inside this class!
+    /// </summary>
+    public class Game
+    {
+        // Place your variables here:
+
+
+        /// <summary>
+        ///     Setup runs once before the game loop begins.
+        /// </summary>
+        public void Setup()
+        {
+            Window.SetTitle("Yippee wahoo game!"); //edit name later DO NOT FORGET.
+            Window.SetSize(800,800);
+            Window.TargetFPS = 60;
+            Window.ClearBackground(Color.Cyan); //placeholder colour since idk what i wanna make yet
+        }
+
+        /// <summary>
+        ///     Update runs every frame.
+        /// </summary>
+        public void Update()
+        {
+
+        }
+    }
+
+}
